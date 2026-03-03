6c63fd2d002e983d598e033474064999880659f1
Mingming Sun <sunmingming@gmail.com>
2026-03-03 12:27:10 +0800

    fix teswt

--- src/sam3d/tests/test_load_models.py
+++ src/sam3d/tests/test_load_models.py
@@ -1,28 +1,36 @@
+import os
 import numpy as np
 from PIL import Image
 from sam3d import SAM3D
 
-sam3d = SAM3D.from_defaults(
-    workspace_dir="/home/sun/Projects/embody/models",
-    device="cuda",
-)
+def test_load_models(workspace_dir):
+    sam3d = SAM3D.from_defaults(
+        workspace_dir=workspace_dir,
+        device="cuda",
+    )
 
-# 4.1 人体重建
-body_result = sam3d.predict(
-    task="body",
-    image="./image_body.png",
-)
-print(len(body_result["instances"]))
+    current_dir = os.path.dirname(os.path.abspath(__file__))
 
-# 4.2 物体重建
-image = np.asarray(Image.open("./image_objects.png"))
-h, w = image.shape[:2]
-mask = np.zeros((h, w), dtype=bool)
-mask[h // 4 : 3 * h // 4, w // 4 : 3 * w // 4] = True
-obj_result = sam3d.predict(
-    task="objects",
-    image=image,
-    mask=mask,
-    seed=42,
-)
-obj_result["gaussian_splat"].save_ply("object.ply")
+    # 4.1 人体重建
+    image_body_path = os.path.join(current_dir, "image_body.png")
+    body_result = sam3d.predict(
+        task="body",
+        image=image_body_path,
+    )
+    print(len(body_result["instances"]))
+
+    # 4.2 物体重建
+    image_objects_path = os.path.join(current_dir, "image_objects.png")
+    image = np.asarray(Image.open(image_objects_path))
+    h, w = image.shape[:2]
+    mask = np.zeros((h, w), dtype=bool)
+    mask[h // 4 : 3 * h // 4, w // 4 : 3 * w // 4] = True
+    obj_result = sam3d.predict(
+        task="objects",
+        image=image,
+        mask=mask,
+        seed=42,
+    )
+    
+    object_ply_path = os.path.join(current_dir, "object.ply")
+    obj_result["gaussian_splat"].save_ply(object_ply_path)
